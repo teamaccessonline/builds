e22e07e903f3ee11446e58a9a873755f326dcf1e
admin <admin@f5lab.local>
2020-05-04 11:41:08 -0400

    Committer: Jason Wilburn <yahshun@yahoo.com>
    New APIs
    
    Delete user
    Check for next available address
    Checkout address
    Checkin address

---
+++ powershell/ad_api/endpoints/DELETE/delete-user.ps1
@@ -0,0 +1,42 @@
+﻿<#
+	.DESCRIPTION
+		This script deletes a user account from Active Directory
+	.EXAMPLE
+        deletee-user.ps1 -RequestArgs  useridentity=user3
+	.NOTES
+        This will return a status message of Success or Failure
+#>
+
+param(
+    $RequestArgs
+    
+)
+ 
+ if (!$RequestArgs) {
+  return "useriidentity parameter must be supplied in query string. Example https://10.1.20.6/user?useridentity="
+
+ }
+
+ $Property, $user = $RequestArgs.split("=")
+
+ Remove-ADUser $user -Confirm:$False
+ if ($?) {
+    $status = "Success"
+    $jsonresponse = New-Object -TypeName psobject
+    $jsonresponse| Add-Member -MemberType NoteProperty -Name status -Value $status
+    $jsonresponse| Add-Member -MemberType NoteProperty -Name useridentity -Value $user
+
+ 
+ 	$Message = $jsonresponse | Select-Object status, useridentity
+ } else {
+      $status = "Fail"
+    $jsonresponse = New-Object -TypeName psobject
+    $jsonresponse| Add-Member -MemberType NoteProperty -Name status -Value $status
+    $jsonresponse| Add-Member -MemberType NoteProperty -Name useridentity -Value $user
+
+ 
+ 	$Message = $jsonresponse | Select-Object status, useridentity
+ }
+ return $Message
+ 
+ 
